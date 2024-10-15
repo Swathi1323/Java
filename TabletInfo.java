24d9bbaa9af62b27ef80e008a7238dab837eca37
Swathi1323 <swathinairudma@gmail.com>
2024-10-15 09:56:35 +0530

    Constructors examples

---
+++ TabletInfo.java
@@ -0,0 +1,26 @@
+public class TabletInfo
+{
+	public static void main(String[] args)
+	{
+	Tablet tab=new Tablet("apple",12.9f,3000,128);
+	Tablet tab1=new Tablet("Samsung",14.6f,5000,256);
+	Tablet tab2=new Tablet("Microsoft",13f,11200,512);
+	Tablet tab3=new Tablet("Lenovo",11.2f,8600,128);
+	Tablet tab4=new Tablet("Amazon",10.1f,10050,256);
+	Tablet tab5=new Tablet("Xiaomi",11f,8600,128);
+	Tablet tab6=new Tablet("Asus",9.7f,5900,512);
+	Tablet tab7=new Tablet();
+	Tablet tab8=new Tablet();
+	Tablet tab9=new Tablet();
+	tab.printDetails();
+	tab1.printDetails();
+	tab2.printDetails();
+	tab3.printDetails();
+	tab4.printDetails();
+	tab5.printDetails();
+	tab6.printDetails();
+	tab7.printDetails();
+    tab8.printDetails();
+    tab9.printDetails();
+}
+}
