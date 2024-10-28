71696e60888a8b8bdacd3ef27e14dec1065e749c
Swathi1323 <swathinairudma@gmail.com>
2024-10-28 12:11:46 +0530

    Constructor chaining

---
+++ Employee.java
@@ -0,0 +1,8 @@
+public class Employee
+{
+	public static void main(String[] args)
+	{
+	EmployeeDetails employeeDetails=new EmployeeDetails(1,"Swathi","Kasaragod","8113822784",23);
+	employeeDetails.printDetails();
+	}
+}
