396c31b342c1a3b0d673dacdfed47af25350ba7a
Swathi1323 <swathinairudma@gmail.com>
2024-10-29 21:36:35 +0530

    Inheritance

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public class Animal
+{
+    String breed;
+    int price;
+    int weight;
+
+    public void makeSound()
+    {
+        System.out.println("Animal is making sound");
+    }
+
+}
+
+
